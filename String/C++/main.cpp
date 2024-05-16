bec75d3826c5f59cd63174dbf6897b0d7c80df6f
H-Matthews <hmatthews070@gmail.com>
2024-05-15 22:37:40 -0500

    Added a gitignore. Tested strings further

--- String/C++/main.cpp
+++ String/C++/main.cpp
@@ -3,15 +3,20 @@
 
 int main()
 {
-    MyString s1("Test");
+    MyString s1("Harrison");
+    MyString s2 = " Matthews";
+    MyString s3;
 
-    std::cout << "Printing MyString Length: " << s1.length() << std::endl;
-    std::cout << "Printing MyString Buffer: " << s1.buffer() << std::endl;
+    std::cout << "Printing s1 Length: " << s1.length() << std::endl;
+    std::cout << "Printing s1 Buffer: " << s1.buffer() << std::endl;
 
-    s1 = "Harrison";
+    std::cout << "Printing s2 Length: " << s2.length() << std::endl;
+    std::cout << "Printing s2 Buffer: " << s2.buffer() << std::endl;
 
-    std::cout << "Printing MyString Length: " << s1.length() << std::endl;
-    std::cout << "Printing MyString Buffer: " << s1.buffer() << std::endl;
+    s3 = s1 + s2;
+
+    std::cout << "Printing s3 Length: " << s3.length() << std::endl;
+    std::cout << "Printing s3 Buffer: " << s3.buffer() << std::endl;
 
     return 0;
 }
